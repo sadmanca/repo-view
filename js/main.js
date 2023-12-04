6882fcf06a3243f44ff5df289d117f2f532f244a
Sadman Hossain <m.sadman.h@gmail.com>
2023-12-04 09:45:05 -0500

    Update mime

--- js/main.js
+++ js/main.js
@@ -40,7 +40,7 @@ function addBtnClone() {
 
 import { Octokit } from "https://esm.sh/@octokit/core";
 import mime from "https://cdn.skypack.dev/mime/lite";
-mime._types.py = 'application/x-python-code'
+// mime._types.py = 'application/python'
 
 marked.setOptions({
   highlight: (code, lang) => {
